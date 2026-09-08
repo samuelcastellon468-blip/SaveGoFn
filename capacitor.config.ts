023ff0a1031ea1f20be4f8e5085256e184dcbd08
samuelcastellon468-blip <samuelcastellon468@gmail.com>
2026-09-08 16:23:27 -0600

    Habilitar CapacitorHttp para evitar bloqueo CORS en peticiones nativas

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,22 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+/*import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.savego.app',
   appName: 'SaveGo',
   webDir: 'dist/status-saver-ios/browser'
+};export default config; */
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.savego.app',
+  appName: 'SaveGo',
+  webDir: 'dist/status-saver-ios/browser',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
 };
 
 export default config;
