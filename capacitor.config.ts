3e0217aedec68bddfe66db94b5cede463e864ef9
samuelcastellon468-blip <samuelcastellon468@gmail.com>
2026-09-08 18:20:23 -0600

    Corregir descarga de estados con Filesystem.readFile y mostrar miniatura de videos

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,6 +6,20 @@ const config: CapacitorConfig = {
   webDir: 'dist/status-saver-ios/browser'
 };export default config; */
 
+/*import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = {
+  appId: 'com.savego.app',
+  appName: 'SaveGo',
+  webDir: 'dist/status-saver-ios/browser',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
+};
+export default config; */
+// DESPUÉS:
+
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
